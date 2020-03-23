f12b4287757d35ef01d4c3bb4b996b90d4f661c4
Norbert Annus <annus.norbert@gmail.com>
2020-03-23 23:49:21 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# JavaScript Algorithms
+
+## List of algorithms
+
+- Chapter 01 - Algorithms and Their Complexities
+    - [Fast Power](./src/01-algorithms-and-their-complexities/fast-power.js)
---
+++ src/01-algorithms-and-their-complexities/fast-power.js
@@ -0,0 +1,10 @@
+/**
+ * Raise number to the power
+ * 
+ * @param {number} number 
+ * @param {number} power 
+ * @return {number}
+ */
+export function fastPower(number, power) {
+    return number ** power;
+}
